0f611a1c7cfe52e019e725c5017b9e7229d6825c
Biel Stela <biel.stela@gmail.com>
2023-11-03 18:10:37 +0100

    WIP set data source with dropdown select

--- deck/app.js
+++ deck/app.js
@@ -19,6 +19,49 @@ const INITIAL_VIEW_STATE = {
   pitch: 0,
 };
 
+fetch("http://localhost:8000")
+  .then((response) => response.json())
+  .then((tables) => {
+    const tableSelect = document.getElementById("tableSelect");
+    tables.forEach((table) => {
+      const option = document.createElement("option");
+      option.value = table;
+      option.text = table;
+      tableSelect.appendChild(option);
+    });
+  })
+  .catch((error) => console.error("Error:", error));
+
+document.getElementById("tableSelect").addEventListener("change", function () {
+  let table = this.value;
+
+  // Clear the columnSelect dropdown
+  const columnSelect = document.getElementById("columnSelect");
+  while (columnSelect.firstChild) {
+    columnSelect.removeChild(columnSelect.firstChild);
+  }
+
+  // Fetch the columns for the new table and populate the columnSelect dropdown
+  fetch(`http://localhost:8000/${table}`)
+    .then((response) => response.json())
+    .then((columns) => {
+      columns.forEach((column) => {
+        const option = document.createElement("option");
+        option.value = column;
+        option.text = column;
+        if (column === "value") {
+          option.selected = true;
+        }
+        columnSelect.appendChild(option);
+      });
+    })
+    .catch((error) => console.error("Error:", error));
+
+  deck.setProps({
+    layers: [...layers.slice(0, -1), createH3TileLayer(table, column)],
+  });
+});
+
 let table = document.getElementById("tableSelect").value;
 let column = document.getElementById("columnSelect").value;
 
@@ -47,10 +90,14 @@ const deck = new Deck({
 
 document.getElementById("tableSelect").addEventListener("change", function () {
   table = this.value;
-  deck.setProps({ layers: [...layers, createH3TileLayer(table, column)] });
+  deck.setProps({
+    layers: [...layers.slice(0, -1), createH3TileLayer(table, column)],
+  });
 });
 
 document.getElementById("columnSelect").addEventListener("change", function () {
   let column = this.value;
-  deck.setProps({ layers: [...layers, createH3TileLayer(table, column)] });
+  deck.setProps({
+    layers: [...layers.slice(0, -1), createH3TileLayer(table, column)],
+  });
 });
